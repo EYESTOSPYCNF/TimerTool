7a4b756a64215fc7580c29c45259d2cad04b81b3
SPYCNF <eyestospycnf@gmail.com>
2024-06-19 01:50:09 +0200

    Update Program.cs

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -1,31 +1,37 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Tebjan Halm
- * Date: 21.01.2014
- * Time: 16:55
- * 
- * 
- */
 using System;
-using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace TimerTool
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
-	internal sealed class Program
-	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
-		[STAThread]
-		private static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm(args));
-		}
-		
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!IsRunAsAdmin())
+            {
+                RunAsAdmin();
+                return;
+            }
+
+            // Your existing code here...
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static bool IsRunAsAdmin()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static void RunAsAdmin()
+        {
+            var exeName = Process.GetCurrentProcess().MainModule.FileName;
+            var startInfo = new ProcessStartInfo(exeName);
+            startInfo.Verb = "runas";
+            Process.Start(startInfo);
+        }
+    }
 }
